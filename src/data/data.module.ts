b01e9b76014b3feb3c8ae5909fae2b9369ebbd1c
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2023-10-26 10:05:04 -0300

    Configuração git

---
+++ src/data/data.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  providers: [
+
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ]
+})
+export class DataModule { }
